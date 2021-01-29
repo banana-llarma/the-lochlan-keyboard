dea41175cd18bf0f39ef016af2ecc3edc193b48b
banana_llarma <malachithomas110@gmail.com>
2021-01-29 19:35:16 +0000

    commit 7

---
+++ template/rules.mk
@@ -0,0 +1,25 @@
+# MCU name
+MCU = atmega32u4
+
+# MCU flasher
+BOOTLOADER = atmel-dfu
+
+# Audio control and System control
+EXTRAKEY_ENABLE = yes
+
+# USB N-Key Rollover
+NKRO_ENABLE = yes
+
+# Enable keyboard underlight functionality
+RGBLIGHT_ENABLE = yes
+
+# Enable keyboard backlight functionality
+BACKLIGHT_ENABLE = yes
+
+# Enable unicode
+UNICODE_ENABLE = yes
+
+# Keyboard layouts
+LAYOUTS = 65_ansi 65_iso 65_ansi_blocker 65_ansi_blocker_split_bs
+
+
